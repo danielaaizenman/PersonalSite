cee9649f005124a1f3d115d80e281f767ffe1466
danielaaizenman <daaizenman@gmail.com>
2023-10-05 14:29:24 -0500

    initial commit

---
+++ DA_ExtraCredit/DA_ExtraCredit/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+using System.IO;
+namespace DA_ExtraCredit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        //TODO: Create a new action method for the new page
+        public IActionResult ATXRecommendations()
+        {
+            return View();
+        }
+
+
+        public IActionResult Resume()
+        {
+            string path = _environment.WebRootPath + "/files/Aizenman.Daniela.Resume.pdf";
+            var stream = new FileStream(path, FileMode.Open);
+            return File(stream, "application/pdf", "Aizenman.Daniela.Resume.pdf");
+        }
+    }
+}
+
+
